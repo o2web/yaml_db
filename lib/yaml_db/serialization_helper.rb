3af37d2dc4f9b6bf66a34ca85425a07537b09db2
Guillaume Loubier <gloubier@o2web.ca>
2016-02-02 11:32:32 -0500

    Delete table if truncate fails because of foreign_key

--- lib/yaml_db/serialization_helper.rb
+++ lib/yaml_db/serialization_helper.rb
@@ -64,8 +64,10 @@ module YamlDb
 
       def self.truncate_table(table)
         begin
-          ActiveRecord::Base.connection.execute("TRUNCATE #{Utils.quote_table(table)}")
+          ActiveRecord::Base.connection.execute("SAVEPOINT before_truncate")
+          ActiveRecord::Base.connection.execute("TRUNCATE #{Utils.quote_table(table)} CASCADE")
         rescue Exception
+          ActiveRecord::Base.connection.execute("ROLLBACK TO SAVEPOINT before_truncate")
           ActiveRecord::Base.connection.execute("DELETE FROM #{Utils.quote_table(table)}")
         end
       end
